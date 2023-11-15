b83aa13002114c71844e4501d48eb2717386b640
lispking <lispking@qq.com>
2023-11-15 09:06:06 +0800

    + step 3

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -7,6 +7,10 @@ use axstd::println;
 
 const PLASH_START: usize = 0x22000000;
 
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let load_start = PLASH_START as *const u8;
@@ -30,13 +34,27 @@ fn main() {
 
     println!("Load payload ok!");
 
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+
     println!("Execute app ...");
+    let arg0: u8 = b'A';
     // execute app
     unsafe { core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
         li      t2, {run_start}
         jalr    t2
         j       .",
         run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        abi_num = const SYS_PUTCHAR,
+        in("a0") arg0,
     )}
 }
 
@@ -44,4 +62,14 @@ fn main() {
 fn bytes_to_usize(bytes: &[u8]) -> usize {
     let bytes = &bytes[..8];
     usize::from_be_bytes(bytes.try_into().unwrap())
+}
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
 }
