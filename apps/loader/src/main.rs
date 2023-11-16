d2aae3e751132c29fef3a1eb26a30f2ebb32f203
lispking <lispking@qq.com>
2023-11-16 15:19:34 +0800

    + exercise 4

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -3,7 +3,7 @@
 #![feature(asm_const)]
 
 #[cfg(feature = "axstd")]
-use axstd::println;
+use axstd::{println, process::exit};
 
 const PLASH_START: usize = 0x22000000;
 
@@ -11,6 +11,29 @@ const PLASH_START: usize = 0x22000000;
 // SBI(0x80000000) -> App <- Kernel(0x80200000)
 // 0xffff_ffc0_0000_0000
 const RUN_START: usize = 0xffff_ffc0_8010_0000;
+
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
+fn abi_terminate(exit_code: i32) {
+    println!("[ABI:Terminate] Terminate Apps!");
+    exit(exit_code);
+}
 
 struct ImageHeader{
     ptr_len: usize
@@ -45,17 +68,41 @@ fn main() {
         run_code.copy_from_slice(app_header.content);
         println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
-        println!("Execute App_{i} ...");
-        // execute app
-        unsafe { core::arch::asm!("
-            li      t2, {run_start}
-            jalr    t2",
-            run_start = const RUN_START,
-        )}
-        println!("Execute App_{i} done\n");
+        // println!("Execute App_{i} ...");
+        // // execute app
+        // unsafe { core::arch::asm!("
+        //     li      t2, {run_start}
+        //     jalr    t2",
+        //     run_start = const RUN_START,
+        // )}
+        // println!("Execute App_{i} done\n");
     });
 
     println!("Load {app_num} app to payload ok!");
+    
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    register_abi(SYS_TERMINATE, abi_terminate as usize);
+
+    let arg0: u8 = b'A';
+    // execute app
+    unsafe { core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+        run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        // abi_num = const SYS_PUTCHAR,
+        abi_num = const SYS_TERMINATE,
+        in("a0") arg0,
+    )}
 }
 
 impl ImageHeader {
