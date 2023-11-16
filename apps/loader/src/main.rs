00bc7876c2650d68f46bd6090d93ed8ae7b92f15
lispking <lispking@qq.com>
2023-11-16 14:39:53 +0800

    + exercise 3

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,10 +1,16 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
+#![feature(asm_const)]
 
 #[cfg(feature = "axstd")]
 use axstd::println;
 
 const PLASH_START: usize = 0x22000000;
+
+// app running aspace
+// SBI(0x80000000) -> App <- Kernel(0x80200000)
+// 0xffff_ffc0_0000_0000
+const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
 struct ImageHeader{
     ptr_len: usize
@@ -24,7 +30,7 @@ fn main() {
 
     let app_num = image_header.load_app_num(PLASH_START);
 
-    println!("Load {app_num} app to payload...");
+    println!("Load {app_num} app to payload...\n");
 
     let mut app_start = PLASH_START + ptr_len;
     (0..app_num).for_each(|i| {
@@ -32,6 +38,21 @@ fn main() {
         app_start = app_header.start + app_header.size;
 
         println!("App_{i} start: {:#x} size: {} content: {:?}", app_header.start, app_header.size, app_header.content);
+
+        let run_code = unsafe {
+            core::slice::from_raw_parts_mut(RUN_START as *mut u8, app_header.size)
+        };
+        run_code.copy_from_slice(app_header.content);
+        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+
+        println!("Execute App_{i} ...");
+        // execute app
+        unsafe { core::arch::asm!("
+            li      t2, {run_start}
+            jalr    t2",
+            run_start = const RUN_START,
+        )}
+        println!("Execute App_{i} done\n");
     });
 
     println!("Load {app_num} app to payload ok!");
